3db78b0e28d21fed4d5b8afc4ad7ef6ef727df5c
Aditya Lesmana <adityalesmana@Adityas-MacBook-Pro.local>
2020-10-07 12:07:17 +0700

    fourth commit

--- Nomor_3/crud_mahasiswa/app/Http/Controllers/MahasiswaController.php
+++ Nomor_3/crud_mahasiswa/app/Http/Controllers/MahasiswaController.php
@@ -8,31 +8,66 @@ use \App\Models\Mahasiswa;
 class MahasiswaController extends Controller
 {
     public function index(){
-      $data_mahasiswa = Mahasiswa::all();
-      return view('mahasiswa.index', ['data_mahasiswa' => $data_mahasiswa]);
+      /**$data_mahasiswa = Mahasiswa::all();
+      return view('mahasiswa.index', ['data_mahasiswa' => $data_mahasiswa]);**/
+      return Mahasiswa::all();
     }
 
-    public function create(Request $request){
-      Mahasiswa::create($request->all());
-      return redirect('/mahasiswa')->with('sukses', 'Data berhasil ditambahkan');
+    public function create(request $request){
+      /**Mahasiswa::create($request->all());
+      return redirect('/mahasiswa')->with('sukses', 'Data berhasil ditambahkan');**/
+      $mahasiswa = new Mahasiswa;
+      $mahasiswa->nim = $request->nim;
+      $mahasiswa->nama = $request->nama;
+      $mahasiswa->gender = $request->gender;
+      $mahasiswa->tempat_lahir = $request->tempat_lahir;
+      $mahasiswa->tanggal_lahir = $request->tanggal_lahir;
+      $mahasiswa->alamat = $request->alamat;
+      $mahasiswa->tahun_masuk = $request->tahun_masuk;
+      $mahasiswa->save();
+
+      return "Data created";
     }
 
-    public function edit($id){
+    /**public function edit($id){
       $mahasiswa = Mahasiswa::find($id);
       return view('mahasiswa/edit', ['mahasiswa' => $mahasiswa]);
-    }
+    }**/
 
-    public function update(Request $request, $id){
-      $mahasiswa = Mahasiswa::find($id);
+    public function update(request $request, $id){
+      /**$mahasiswa = Mahasiswa::find($id);
       $mahasiswa->update($request->all());
 
-      return redirect('/mahasiswa')->with('sukses', 'Data berhasil diubah');
+      return redirect('/mahasiswa')->with('sukses', 'Data berhasil diubah');**/
+      $nim = $request->nim;
+      $nama = $request->nama;
+      $gender = $request->gender;
+      $tempat_lahir = $request->tempat_lahir;
+      $tanggal_lahir = $request->tanggal_lahir;
+      $alamat = $request->alamat;
+      $tahun_masuk = $request->tahun_masuk;
+
+      $mahasiswa = Mahasiswa::find($id);
+      $mahasiswa->nim = $nim;
+      $mahasiswa->nama = $nama;
+      $mahasiswa->gender = $gender;
+      $mahasiswa->tempat_lahir = $tempat_lahir;
+      $mahasiswa->tanggal_lahir = $tanggal_lahir;
+      $mahasiswa->alamat = $alamat;
+      $mahasiswa->tahun_masuk = $tahun_masuk;
+      $mahasiswa->save();
+
+      return "Data updated";
     }
 
     public function delete($id){
-      $mahasiswa = Mahasiswa::find($id);
+      /**$mahasiswa = Mahasiswa::find($id);
       $mahasiswa->delete($mahasiswa);
 
-      return redirect('/mahasiswa')->with('sukses', 'Data berhasil dihapus');
+      return redirect('/mahasiswa')->with('sukses', 'Data berhasil dihapus');**/
+      $mahasiswa = Mahasiswa::find($id);
+      $mahasiswa->delete();
+
+      return "Data deleted";
     }
 }
--- Nomor_3/crud_mahasiswa/routes/api.php
+++ Nomor_3/crud_mahasiswa/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::post('/mahasiswa/create', [MahasiswaController::class, 'create']);
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
+Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'delete']);
